0923b284f899b04019db2a3c380ba60549742db3
meir <meir@gmail.com>
2018-04-24 18:23:45 +0300

    try2

--- public/script.js
+++ public/script.js
@@ -1,30 +1,33 @@
 // an array with all of our cart items
 var cart = [];
-
+var total=0;
 var view=false;
 var updateCart = function () {
   // TODO: Write this function. In this function we render the page.
   // Meaning we make sure that all our cart items are displayed in the browser.
   // Remember to empty the "cart div" before you re-add all the item elements.
-  var total=0;
- cart.forEach(element => {
-  total+=element.price
-});
-  $('.total').text(total)
-}
+    total=0;
+    cart.forEach(element => {
+          if(element!="")
+          {
+            total+=element.price
+          }
+      });
+    $('.total').text(total)
+  }
+///test
 
 
 var addItem = function (item) {
   // TODO: Write this function. Remember this function has nothing to do with display. 
   // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
-  $('.cart-list').append('<p>'+item.name+' -  '+item.price+'')
+  $('.cart-list').append('<p>'+item.name+' - '+item.price+' <button type="button"  data-length="'+cart.length+'" class="btn clear-item  ">Clear item</button>')
+  clertThis()
 }
 
 var clearCart = function () {
   // TODO: Write a function that clears the cart ;-)
   cart = [];
-  $('.cart-list').empty();
-  // $('.total').text('0')
   updateCart();
 
 }
@@ -56,7 +59,7 @@ $('.add-to-cart').on('click', function () {
  }
  cart.push(item)
   addItem(item);
-  updateCart();//?
+  updateCart();
 });
 
 $('.clear-cart').on('click', function () {
@@ -65,3 +68,42 @@ $('.clear-cart').on('click', function () {
 
 // update the cart as soon as the page loads!
 updateCart();
+var  clertThis= function clear_item(){
+  $('.clear-item').off()
+  $('.clear-item').on('click', function () {
+      var num=$(this).data().length-1;// המיקום במערך
+      cart[num]="";
+      $(this).closest('p').remove()// מחיקה מהתצוגה
+      updateCart(); //הזמנת עיכון העגלה
+  });
+}
+$('.m2').on('click', function () {
+debugger
+$('.cart-list').empty();
+cart.sort()
+for(var i=0;i<cart.length;i++)
+{
+var numq=0;
+if(cart[i]!="")
+{
+    var m_item=cart[i].name;
+    while(i<cart.length &&m_item==cart[i].name) 
+    {
+      numq++;
+      i++;
+    }
+    if(numq==0)
+    {
+      $('.cart-list').append('<p>'+item.name+' - '+item.price+' <button type="button"  data-length="'+cart.length+'" class="btn clear-item  ">Clear item</button>')
+    }
+    else{
+      $('.cart-list').append('<p>'+item.name+' - '+item.price+'('+numq+') <button type="button"  data-length="'+cart.length+'" class="btn clear-item  ">Clear item</button>')
+    }
+    i--;
+    /// ממציר(כמה)
+}
+
+clertThis()
+
+}
+});
